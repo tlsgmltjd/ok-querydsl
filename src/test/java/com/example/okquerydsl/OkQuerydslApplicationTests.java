d70431970465c92a8e793e5ea39cb247c815198d
신희성 <vvwv3092@gmail.com>
2024-08-06 17:49:06 +0900

    Add db, jpa

--- src/test/java/com/example/okquerydsl/OkQuerydslApplicationTests.java
+++ src/test/java/com/example/okquerydsl/OkQuerydslApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.okquerydsl;
 
+import com.example.okquerydsl.entity.Hello;
+import com.example.okquerydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.okquerydsl.entity.QHello.*;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional // 테스트에 트랜잭션을 적용하면 테스트 이후에는 default로 rollback함
 class OkQuerydslApplicationTests {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello helloEntity = new Hello();
+        em.persist(helloEntity);
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        Hello result = queryFactory
+                .selectFrom(hello)
+                .fetchOne();
+
+        assertThat(result.getId()).isEqualTo(helloEntity.getId());
     }
 
 }
