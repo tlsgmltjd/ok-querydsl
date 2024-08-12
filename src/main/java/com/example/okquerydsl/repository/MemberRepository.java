0749d0bbdef617c35e8addd665d7889edc3b71ed
신희성 <vvwv3092@gmail.com>
2024-08-13 01:01:52 +0900

    QuerydslPredicateExecutor

--- src/main/java/com/example/okquerydsl/repository/MemberRepository.java
+++ src/main/java/com/example/okquerydsl/repository/MemberRepository.java
@@ -2,11 +2,14 @@ package com.example.okquerydsl.repository;
 
 import com.example.okquerydsl.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
 // MemberRepository <<interface>> -- extends --> JpaRepository <<interface>>, MemberCustomRepository <<interface>> (MemberRepositoryImpl)
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
+// QuerydslPredicateExecutor을 JPARepository에서 extends 받아서 사용할 수 있다.
+// SpringDataJpa의 method들 파라미터에 Querydsl의 Q-Class의 where절 조건문을 넘겨서 필터링 하는것이 가능하다.
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository, QuerydslPredicateExecutor<Member> {
     List<Member> findByUsername(String username);
 }
--- src/test/java/com/example/okquerydsl/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/okquerydsl/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.okquerydsl.repository;
 import com.example.okquerydsl.dto.MemberSearchCondition;
 import com.example.okquerydsl.dto.MemberTeamDto;
 import com.example.okquerydsl.entity.Member;
+import com.example.okquerydsl.entity.QMember;
 import com.example.okquerydsl.entity.Team;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static com.example.okquerydsl.entity.QMember.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -131,5 +133,34 @@ class MemberRepositoryTest {
         assertThat(result.getContent())
                 .extracting("username")
                 .containsExactly("member1", "member2", "member3");
+    }
+
+    @Test
+    void queryDslPredicateExecutorTest() {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+
+        Member member3 = new Member("member3", 10, teamB);
+        Member member4 = new Member("member4", 10, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // QuerydslPredicateExecutor 한계점
+        // 묵시적 조인은 가능하지만 명시적 조인은 불가능하다.
+        // repository의 클라이언트가 Q-Class를 의존해서 조건절을 넘겨야하기 때문에 querydsl을 의존해야한다.
+        // 복잡한 환경에서는 한계가 명확하다
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 99).and(member.username.eq("member1")));
+        for (Member m : result) {
+            System.out.println("m = " + m);
+        }
     }
 }
